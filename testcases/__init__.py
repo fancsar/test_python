ec8507bda2f349fa3672b092ba2ab1160e0122cd
fancsar <m13821774377@163.com>
2019-08-21 22:49:24 +0800

    add testcases

---
+++ testcases/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#@Time   : 2019/4/2011:38
+#@Author :lemon_FCC
+#@Email  :670992243@qq.com
+#@File   :__init__.py.py 
---
+++ testcases/test_recharge.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#@Time   : 2019/4/2111:37
+#@Author :lemon_FCC
+#@Email  :670992243@qq.com
+#@File   :test_bidloan.py
