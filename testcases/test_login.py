c23dd95a1f61f01503a5d5b131e87fbebb3e46a9
fancsar <m13821774377@163.com>
2019-12-15 21:05:48 +0800

    新增代码

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -12,7 +12,7 @@ from API_01.common.do_contants import case_file, case_files
 from ddt import ddt, data, unpack
 
 
-#跑单个模块用例时，使用
+# 跑单个模块用例时，使用
 #
 # class LoginTest(unittest.TestCase):
 #     def setUp(self):
@@ -38,20 +38,21 @@ from ddt import ddt, data, unpack
 #         self.http_request.close()
 
 # =============================================================================\
-#多个模块用例进行集合，进行测试
+# 多个模块用例进行集合，进行测试
 # casess=[]
 # for i in ['login', 'recharge']:
 #     print(type(i))
 #     doexcel = DoExcel(case_file, i)
 #     cases = doexcel.get_case()
 #     casess.extend(cases)
-#优化后的单个用例（充值接口）
+# 优化后的单个用例（充值接口）
 @ddt
 class LoginTest(unittest.TestCase):
     # 写到类属性中
     doexcel = DoExcel(case_files, 'recharge')
     cases = doexcel.get_case()
-    #转换为类方法，即不用创建对象
+
+    # 转换为类方法，即不用创建对象
     @classmethod  # 用classmethod 装饰类，运行类时启动一次,用一个http请求保存session
     def setUpClass(cls):
         # 创建http请求对象session
@@ -70,10 +71,14 @@ class LoginTest(unittest.TestCase):
             self.doexcel.write_case(case.case_id + 1, resp.text, 'FAIL')
             raise e
 
-    @classmethod  #修饰为类方法时方法后加Class、
+    @classmethod  # 修饰为类方法时方法后加Class、
     def tearDownClass(cls):
         # 关闭session
         cls.http_request.close()
 
         # 注意：在test方法中运行时，会报错（这个方法加载不了类），
         #      因为用了ddt，所以一定要从类开始加载
+
+
+# git 提交练习
+print('git提交练习')
